406490b9c7644a65e78fd99c27194953694d2df3
oceanchiou <40818899+oceanchiou@users.noreply.github.com>
2018-08-01 10:34:53 +0800

    Update checkfile.py

--- checkfile.py
+++ checkfile.py
@@ -5,10 +5,10 @@ import time
 
 filepath = sys.argv[1]
 LogName = os.listdir(filepath)
-Expect_LogName = ["1098R20_SDK_sdk_nvme_ramdrive_debug.log", "ASIC_NVME_Ramdisk_0.log", "C1_ATCM.log", "PROGRAM0"]
+Expect_LogName = ["1098R20_SDK_sdk_nvme_ramdrive_debug.axf", "ASIC_NVME_Ramdisk_0.lst", "C1_ATCM", "PROGRAM0"]
 
 if __name__ == "__main__":	
-	f = open(filepath +"/Build/summary.log","w")	         
+	f = open(filepath + "/summary.log","w")	         
 	for LogNameinBuildStatus in Expect_LogName:
 		#print "(filepath + LogNameinBuildStatus) ->", (filepath + LogNameinBuildStatus)
 		if os.path.exists(filepath + LogNameinBuildStatus):
